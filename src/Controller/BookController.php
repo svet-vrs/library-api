ada1c48b334cd152f91bffba9269c4fe054cd816
svet-vrs <svetlovevlad@gmail.com>
2023-01-17 01:39:20 +0200

    exceptions

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -5,12 +5,11 @@ namespace App\Controller;
 use App\Model\BookListResponse;
 use App\Service\BookService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Exception\BookCategoryNotFoundException;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use OpenApi\Annotations as OA;
 use Nelmio\ApiDocBundle\Annotation\Model;
+use App\Model\ErrorResponse;
 
 class BookController extends AbstractController
 {
@@ -24,14 +23,15 @@ class BookController extends AbstractController
      *     description="Returns books inside a category",
      *     @Model(type=BookListResponse::class)
      * )
+     * @OA\Response(
+     *     response=404,
+     *     description="book category not found",
+     *     @Model(type=ErrorResponse::class)
+     * )
      */
-    #[Route(path: '/api/v1/category/{id}/books')]
+    #[Route(path: '/api/v1/category/{id}/books', methods: ['GET'])]
     public function booksByCategory(int $id): Response
     {
-        try {
-            return $this->json($this->bookService->getBooksByCategory($id));
-        } catch (BookCategoryNotFoundException $exception) {
-            throw new HttpException($exception->getCode(), $exception->getMessage());
-        }
+        return $this->json($this->bookService->getBooksByCategory($id));
     }
 }
--- tests/Service/BookServiceTest.php
+++ tests/Service/BookServiceTest.php
@@ -3,7 +3,6 @@
 namespace App\Tests\Service;
 
 use App\Entity\Book;
-use App\Entity\BookCategory;
 use App\Exception\BookCategoryNotFoundException;
 use App\Model\BookListItem;
 use App\Model\BookListResponse;
@@ -21,9 +20,9 @@ class BookServiceTest extends AbstractTestCase
         $bookRepository = $this->createMock(BookRepository::class);
         $bookCategoryRepository = $this->createMock(BookCategoryRepository::class);
         $bookCategoryRepository->expects($this->once())
-            ->method('find')
+            ->method('existsById')
             ->with(130)
-            ->willThrowException(new BookCategoryNotFoundException());
+            ->willReturn(false);
 
         $this->expectException(BookCategoryNotFoundException::class);
 
@@ -40,9 +39,9 @@ class BookServiceTest extends AbstractTestCase
 
         $bookCategoryRepository = $this->createMock(BookCategoryRepository::class);
         $bookCategoryRepository->expects($this->once())
-            ->method('find')
+            ->method('existsById')
             ->with(130)
-            ->willReturn(new BookCategory());
+            ->willReturn(true);
 
         $service = new BookService($bookRepository, $bookCategoryRepository);
         $expected = new BookListResponse([$this->createBookItemModel()]);
